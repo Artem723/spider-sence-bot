9b5a56213e0ecd96e6b2e6b88cb4da5f4353dd82
Artsiom Sauchuk <artemsavchuk01@gmail.com>
2019-06-03 12:09:06 +0200

    Added ability to notify multiple users

--- handlers.js
+++ handlers.js
@@ -26,13 +26,15 @@ function createNotificationServerHandler(bot) {
 
         if (req.method === 'GET' && pathname === '/notification' && query && query.device_id) {
             db.getDocByDeviceId(query.device_id, (doc) => {
-                if (doc) {
-                    doc.chat_id && bot.sendMessage(doc.chat_id, `❗`);
+                if (doc) { // send message to moltiple receivers with chat id
+                    doc.chat_ids && doc.chat_ids.forEach(id => {
+                        bot.sendMessage(id, `❗`);
+                    });
                     res.writeHead(200, { "Content-Type": "text/plain" });
-                    res.end(doc.chat_id ? 'Ok, notified!' : 'Client is not connected!');
+                    res.end(doc.chat_ids ? 'Ok, notified!' : 'Client is not connected!');
                 } else {
                     res.writeHead(400, { "Content-Type": "text/plain" });
-                    res.end(`Device with id:${query.device_id}! 😢`);
+                    res.end(`Cannot find device with id:${query.device_id}! 😢`);
                 }
             });
         } else {
--- repository.js
+++ repository.js
@@ -4,6 +4,7 @@ const mongoUser = process.env.MONGO_USER;
 const mongoPWD = process.env.MONGO_PASSWORD;
 const connectionString = `mongodb://${mongoUser}:${mongoPWD}@ds259806.mlab.com:59806/spider-sense-db`;
 const dbName = 'spider-sense-db';
+const colName = 'usersMapping_v2';
 
 const client = new MongoClient(connectionString);
 let db = null;
@@ -12,20 +13,32 @@ let col = null;
 client.connect((err) => {
     console.log("Connected successfully to server");
     db = client.db(dbName);
-    col = db.collection('usersMapping');
+    col = db.collection(colName);
 });
 
 function setChatId(deviceId, chatId, cb) {
-    col && col.findOneAndUpdate({device_id: deviceId}, {$set: {chat_id: chatId}}, (err, r) => {
+    if (!col) {
+        cb(null);
+        return;
+    }
+    getDocByDeviceId(deviceId, (doc) => {
+        if (!doc) {
+            cb(null);
+            return;
+        }
+        console.log('DOC: ', doc);
+        chatIds = doc.chat_ids.concat(chatId);
+        col.findOneAndUpdate({device_id: deviceId}, {$set: {chat_ids: chatIds}}, (err, r) => {
         if (err) {
             console.log(err);
             cb(null);
         } else {
             cb(r);
         }
-    });
-        
+    })});    
 }
+
+// function updateUser(col, doc, deviceId, chat)
 
 function getDocByDeviceId(deviceId, cb) {
     col && col.find({ device_id: deviceId }).limit(1).next((err, doc) => {
